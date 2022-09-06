79d26c68c1132723a9814dda7ae714dd623a9590
David-Helmerson <davidfhelmerson@gmail.com>
2022-09-06 10:42:55 +1000

    cardioid planner node

---
+++ navigation/navigation/cardioid_planner.py
@@ -0,0 +1,70 @@
+import time
+import rclpy
+from rclpy.node import Node
+from navigation.nav_helpers import *
+from geometry_msgs.msg import Twist
+from project_interfaces.msg import RobotPose, SerialCommand, UltrasonicDistances, Waypoint
+from project_interfaces.srv import PoseRequest
+
+class PlannerNode(Node):
+    """
+    Ros integration of given code
+    """
+    def __init__(self):
+        super().__init__('planner')
+        
+        # ROS2 parameters
+        self.declare_parameter('freq', 15.0)
+        self.declare_parameter('obstacle_threshold', 0.05)
+        self.freq = self.get_parameter('freq').get_parameter_value().double_value
+
+        # Important objects
+        self.goal_wp = None
+        self.planner = CardioidPlanner(2/self.freq, 1, 1, 0)  # Plans head for twice the node's operating freq, just incase
+
+        self.pose_client = self.create_client(PoseRequest, 'get_pose')
+        while not self.pose_client.wait_for_service(timeout_sec=0.5):
+            self.get_logger().info('pose server not available, waiting..')
+        self.pose_req = PoseRequest()
+
+        self.goal_sub = self.create_subscription(Waypoint, 'goal_waypoint', self.goal_callback)
+        self.us_sub = self.create_subscription(UltrasonicDistances, 'us_dists', self.us_callback)
+        self.cmd_pub = self.create_publisher(SerialCommand, 'command_send', 10)
+        self.planner_timer = self.create_timer(1/self.freq, self.planner_callback)
+
+    def get_pose(self):
+        self.pose_req.time = time.time()
+        self.future = self.pose_client.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result().pose
+
+    def goal_callback(self, msg):
+        self.goal_wp = msg
+
+    def us_callback(self, msg):
+        thresh = self.get_parameter('object_threshold').get_parameter_value().double_value
+        self.planner.left, self.planner.right = msg.left < thresh, msg.right < thresh
+
+    def planner_callback(self):
+        if self.goal_wp is not None:
+            pose = self.get_pose()
+            px, py = self.planner.plan(self.goal_wp.x, self.goal_wp.y, 0, pose.x, pose.y, pose.th)
+            out_msg = SerialCommand()
+            out_msg.id, out_msg.p1, out_msg.p2 = 101, px, py
+            self.cmd_pub.publish(out_msg)
+
+    
+
+
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    planner = PlannerNode()
+    rclpy.spin(planner)
+    planner.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
