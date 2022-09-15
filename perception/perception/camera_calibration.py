eee8317a2fb65f57e2fc9f59ad7dc3b003fd0c2b
YofukashiNoUta <110396009+YofukashiNoUta@users.noreply.github.com>
2022-09-15 16:03:03 +0800

    camera calibration and depth calculation

---
+++ perception/perception/camera_calibration.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+def get_calib_param()
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob('*.jpg')
+    for fname in images:
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners)
+    cv.destroyAllWindows()
+
+    # Calibration
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return  ret, mtx, dist, rvecs, tvecs, objpoints, imgpoints
+
+def undst(filename, mtx = mtx, dist = dist) #filename in str e.g. 'image.jpg'
+    # Undistortion
+    img = cv.imread(filename)
+    h,  w = img.shape[:2]
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    # undistort
+    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv.imwrite('calibresult.png', dst)
+
+
+def calib_error(objpoints = objpoints, imgpoints = imgpoints, rvecs = rvecs, tvecs = tvecs, mtx = mtx, dist = dist)
+    # Re-projection Error 
+    #the smaller and closer to 0, the better the accuracy
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+        mean_error += error
+    print( "total error: {}".format(mean_error/len(objpoints)) )
+
+
+def matcher(train,query) #train and query filename in str type e.g. 'image.jpg'
+    img_train = cv.imread(train, cv.IMREAD_GRAYSCALE)
+    img_query = cv.imread(query, cv.IMREAD_GRAYSCALE)
+    orb = cv.ORB_create()
+    kp_train, des_train = orb.detectAndCompute(img_train, None)
+    kp_query, des_query = orb.detectAndCompute(img_query, None)
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des_train, des_query)
+    matches = sorted(matches, key=lambda x: x.distance)
+    return matches  #matches is a list which contains queryIdx, trainIdx, distance, imgIdx
+
+
+def triangulation(kp1, kp2, T_1w, T_2w):
+    kp1_3D = np.ones((3, kp1.shape[0]))
+    kp2_3D = np.ones((3, kp2.shape[0]))
+    kp1_3D[0], kp1_3D[1] = kp1[:, 0].copy(), kp1[:, 1].copy()
+    kp2_3D[0], kp2_3D[1] = kp2[:, 0].copy(), kp2[:, 1].copy()
+    X = cv2.triangulatePoints(T_1w[:3], T_2w[:3], kp1_3D[:2], kp2_3D[:2])
+    X /= X[3]
+    X1 = T_1w[:3] @ X
+    X2 = T_2w[:3] @ X
+    return X[:3], X1, X2 
+
+
+
