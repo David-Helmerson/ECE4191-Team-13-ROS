4dca8cc7fe00f6e5f98e9e0951652d7c93544bf1
David-Helmerson <davidfhelmerson@gmail.com>
2022-09-18 16:48:47 +1000

    Merged important changes from prelim implementation to main

--- navigation/navigation/nav_helpers.py
+++ navigation/navigation/nav_helpers.py
@@ -1,4 +1,155 @@
+from re import S
+import time
 import math
+import numpy as np
+
+
+class StupidPlanner:
+    def __init__(self):
+        self.left, self.right = False, False
+        self.ang_thresh = 0.1
+        self.state = 0
+        self.state_2_time = -math.inf
+        self.state_3_time = -math.inf
+        self.v, self.w = 0.1, 0.4
+
+    def plan(self, px, py, pth, gx, gy):
+        
+        if self.state == 0:
+            # Path towards heading
+            if not self.left and not self.right:
+                rx, ry = gx - px, gy - py
+                heading =  (math.atan2(ry, rx) - pth) % (2*math.pi)
+                print(heading)
+                if heading < self.ang_thresh or heading > 2*math.pi - self.ang_thresh: 
+                    return self.v, 0.0
+                elif heading < math.pi: return 0.0, self.w
+                else: return 0.0, -self.w
+
+            # rotate to avoid obstacles
+            elif self.right: 
+                self.state = 1
+                return 0, self.w
+            elif self.left: 
+                self.state = 1
+                return 0, -self.w
+
+        # Enter state 2 when obstacle is out of view
+        elif self.state == 1:
+            if self.right: 
+                return 0, self.w
+            elif self.left: 
+                return 0, -self.w
+            else:
+                self.state = 2
+                self.state_2_time = time.time()
+                return self.v, 0.0
+
+        elif self.state == 2: return self.v, 0.0
+        else: return 0.0, 0.0
+        
+
+class CardioidPlanner:
+    """
+    Planner that determines point on a cardioid closest to the goal point
+
+    Parameters
+    ----------
+    radius: double
+        Cardioid size parameter, in meters
+    n_points: int
+        Number of points equally radially spaced along cardioid to consider
+
+    Attributes
+    ----------
+    left: bool
+        True if left ultrasonic sensor detects object
+    right: bool
+        True if right ultrasonic sensor detects object
+
+    Methods
+    -------
+    plan: (float, float, float), (float, float) --> (float, float)
+        Determines point on cardioid closest to goal point within obstacle detection restrictions
+    
+    """
+    def __init__(self, radius, n_points):
+        # Points on cardioid are given in polar coordinates
+        self.points = [(2*radius*(1 - math.cos(th)), th) 
+                        for th in np.linspace(-math.pi, math.pi, n_points, endpoint=False)]
+        self.left = False
+        self.right = False
+    
+    def plan(self, pose, goal):
+        # Find cardioid point closest to goal, left and right sensor measurements permitting
+        min_dist2, min_point = math.inf, None
+        for p in self.points:
+            if (p[1] <= 0 and self.left) or (p[1] >= 0 and self.right):
+                # Find euclidean distance from cardioid point to goal
+                p_dist2 = (p[0]*math.cos(pose[2]+p[1]) + pose[0] - goal[0])**2 + \
+                         (p[0]*math.sin(pose[2]+p[1]) + pose[0] - goal[0])**2 
+                if p_dist2 < min_dist2: min_dist2, min_point = p_dist2, p
+
+        return min_point[0]*math.cos(min_point[1]), min_point[0]*math.sin(min_point[1])
+
+    def plan_linang(self, pose, goal, dt):
+        px, py = self.plan(pose, goal)
+        return point_to_linang(px, py, dt)
+
+
+class TentaclePlanner:
+    
+    def __init__(self, dt=0.1, steps=5, alpha=1, beta=0.0):
+        
+        self.dt = dt
+        self.steps = steps
+        # Tentacles are possible trajectories to follow
+        self.tentacles = [(0.0,0.5),(0.0,-0.5),(0.05,0.5),(0.05,-0.5),(0.05,0.25),(0.05,-0.25),(0.05,0.0),(0.0,0.0)]
+        
+        self.alpha = alpha
+        self.beta = beta
+        
+        # Status of obstacles detected to the left and right
+        self.left = False 
+        self.right = False
+    
+    # Play a trajectory and evaluate where you'd end up
+    def roll_out(self,v,w,goal_x,goal_y,goal_th,x,y,th):
+        
+        for j in range(self.steps):
+        
+            x = x + self.dt*v*np.cos(th)
+            y = y + self.dt*v*np.sin(th)
+            th = (th + w*self.dt)
+            
+            if (w > 0 and self.left) or (w < 0 and self.right):
+                return np.inf
+        
+        # Wrap angle error -pi,pi
+        e_th = goal_th-th
+        e_th = np.arctan2(np.sin(e_th),np.cos(e_th))
+        
+        cost = self.alpha*((goal_x-x)**2 + (goal_y-y)**2) + self.beta*(e_th**2)
+        
+        return cost
+        
+    
+    # Choose trajectory that will get you closest to the goal
+    def plan(self,goal_x,goal_y,goal_th,x,y,th):
+        
+        costs = []
+        for v,w in self.tentacles:
+            costs.append(self.roll_out(v,w,goal_x,goal_y,goal_th,x,y,th))
+        
+        best_idx = np.argmin(costs)
+        
+        return self.tentacles[best_idx]
+
+
+def point_to_linang(px, py, dt):
+    r, w, = (px**2 + py**2)/(2*px*dt), math.atan2(py, px)
+    return w*r, w
+    
 
 def pp_waypoint(path, pos, dist, max_lookahead=math.inf):
     """
---
+++ navigation/navigation/waypoint_manager.py
@@ -0,0 +1,74 @@
+import time
+import rclpy
+from rclpy.node import Node
+from project_interfaces.msg import Waypoint, RobotPose
+from project_interfaces.srv import PoseRequest
+
+class WaypointManagerNode(Node):
+    """
+    Node that determines next waypoint to be pathed to from a fixed list.
+    Waypoints changes are determined when robot reaches proximity of current goal
+
+    Parameters
+    ----------
+    frequency: double
+        Frequency of goal waypoint publishing
+    distance_thresh: double
+        Distance withing current goal waypoint that will consider it met
+
+    Topics
+    ------
+    goal_waypoint: L{project_interfaces.Waypoint} message
+        Current waypoint for the robot to path to
+
+    Services
+    --------
+    get_pose: L{project_interfaces.PoseRequest} service
+        Queries current time and recieves robot's predicted position based on latest velocity estimate
+
+    """
+    def __init__(self):
+        super().__init__('waypoint_manager')
+
+        # ROS2 Parameters
+        self.declare_parameter('frequency', 10.0)
+        self.declare_parameter('distance_thresh', 0.05)
+        self.freq = self.get_parameter('frequency').get_parameter_value().double_value
+
+        # Important objects
+        self.wp_list = [(0.3, 0.8), (0.9, 0.8)]
+        self.pose = None
+
+        self.pose_sub = self.create_subscription(RobotPose, 'pose_est', self.pose_callback, 10)
+        self.pub = self.create_publisher(Waypoint, 'goal_waypoint', 10)
+        self.timer = self.create_timer(1/self.freq, self.timer_callback)
+        self.msg = Waypoint()
+
+        self.pub_wp()
+
+    def pose_callback(self, msg): 
+        self.pose = msg
+
+    def pub_wp(self):
+        self.msg.x, self.msg.y = self.wp_list[0][0], self.wp_list[0][1]
+        self.pub.publish(self.msg)
+
+    def timer_callback(self):
+        if self.pose is not None:
+            thresh = self.get_parameter('distance_thresh').get_parameter_value().double_value
+            if (self.pose.x-self.wp_list[0][0])**2 + (self.pose.y-self.wp_list[0][1])**2 < thresh**2:
+                del self.wp_list[0]
+        self.pub_wp()
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    waypoint_manager = WaypointManagerNode()
+    rclpy.spin(waypoint_manager)
+    waypoint_manager.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
