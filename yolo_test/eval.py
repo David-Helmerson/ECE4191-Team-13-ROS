5a6f714fb96d523d26e71ea5b2baffdbe6db4904
DavidHelmerson <davidfhelmerson@gmail.com>
2022-10-17 10:23:02 +1100

    yolov5n6 testing

--- yolo_test/eval.py
+++ yolo_test/eval.py
@@ -1,11 +1,16 @@
 import torch
+import time
 from PIL import Image
 
 img = Image.open('image_0.jpg')
 
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='v5b6best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='v5n6best.pt')
 with torch.no_grad():
     df = model(img).pandas().xyxy[0]
+    t0 = time.time()
+    for _ in range(100):
+    	_ = model(img)
+    print((time.time()-t0)/100)
     arr = df[df['class'] == 0].to_numpy()[:, :-3]
     print(arr)
