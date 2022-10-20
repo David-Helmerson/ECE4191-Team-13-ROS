5827c05b8e12e547a03e7bd2bc7bc5f9f49a76b2
David-Helmerson <davidfhelmerson@gmail.com>
2022-10-20 18:29:07 +1100

    better debugging

--- serial_communication/serial_communication/serial_reader.py
+++ serial_communication/serial_communication/serial_reader.py
@@ -76,7 +76,7 @@ class SerialReaderNode(Node):
                 # Publish data to topic
                 self.get_logger().info(str(in_bytes))
                 _, id, p1, p2, _ = struct.unpack('>3sBff3s', in_bytes)
-                self.get_logger.info(str(id) + ' ' + str(p1) + ' ' + str(p2))
+                self.get_logger().info(str(id) + ' ' + str(p1) + ' ' + str(p2))
 
                 # Resynchronize sent serial bytes for PSoC
                 if id == 90: 
